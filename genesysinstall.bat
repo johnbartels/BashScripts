e1fdeb6fa112d2225c009ae4602cc6f88fb96079
John Bartels <johndbartels@gmail.com>
2025-06-03 11:14:33 -0400

    Add files via upload

---
+++ genesysinstall.bat
@@ -0,0 +1,3 @@
+bitsadmin.exe /transfer "GenesysDesktopDownload" https://app.mypurecloud.com/directory-windows/build-assets/2.42.822-127/genesys-cloud-windows-2.42.822.exe C:\Users\Administrator\Desktop\directoryinstaller.exe
+timeout /t 15
+C:\Users\Administrator\Desktop\directoryinstaller.exe /S
